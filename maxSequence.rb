5d7521845b17257050106ca02aead466e4e4bbd6
Captain1965 <alex1965-03@mail.ru>
2021-10-29 21:08:10 +0300

    variant end

--- maxSequence.rb
+++ maxSequence.rb
@@ -2,35 +2,36 @@
 #нахождении максимальной суммы непрерывной подпоследовательности 
 #в массиве или списке целых чисел:
 #encoding UTF-8
-def max_Sequence(arr)
-	index = arr.index(arr.max)
-	arr_left = arr.slice(0,index+1)
-	arr_right = arr.slice(index,arr.size-1)
-	    if arr.delete_if {|item| item <=0}.size == 0
-		return '([],0)'
-	    end
-    if arr_left.sum >= arr_right.sum 
-     return (arr_left) , arr_left.sum
-    else return (arr_right) #arr_right.sum)
-    end	
+
+def max_sequence(arr)
+	    if arr.all?{ |elem| elem <= 0 } 
+		return 0
+	    end 
+	   l = arr.size
+	max_arr_1 = 0
+	l.times do
+		if max_arr_1 < max_arr(arr) then
+			max_arr_1 = max_arr(arr)
+			arr.shift
+		else
+			arr.shift
+		end	
+	end
+	max_arr_1
 end
 def max_arr(arr)
-	arr_help =[]
-	l = arr.size-1
-	i = 2
-	arr_sum = arr[0]
+	l = arr.size
+	i = 1
+	arr_sum = 0
 		l.times do
 		    if arr_sum < arr.slice(0,i).sum then
 		    	arr_sum = arr.slice(0,i).sum
-		    	arr_help = arr.slice(0,i)
 		    	i +=1
+		    else 
+		    	i +=1 
 		   	end
-		   	i +=1 
 		end
-	arr_help.sum
+	arr_sum
 end
+#print max_Sequence([-2, 1, -7, 4, -10, 2, 1, 5, 4])
 
-
-
-#print max_Sequence([-2, 1, -7, 4, -10, 2, 1, 5, 4]
-print max_arr([ -2, -1, 5, 4])
