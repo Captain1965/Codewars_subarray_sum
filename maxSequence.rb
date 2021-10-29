aa899e4c14358df5a8c0d3e3f1ea5909e246f4bf
Captain1965 <alex1965-03@mail.ru>
2021-10-29 05:57:28 +0300

    diferent two subarray

--- maxSequence.rb
+++ maxSequence.rb
@@ -7,35 +7,30 @@ def max_Sequence(arr)
 	arr_left = arr.slice(0,index+1)
 	arr_right = arr.slice(index,arr.size-1)
 	    if arr.delete_if {|item| item <=0}.size == 0
-		return 0
+		return '([],0)'
 	    end
-	    print arr_left
-	    puts
-	    print arr_right
-    #print arr_left = max_arr(arr_left.reverse).reverse 
-    #puts
-    #print arr_right = max_arr(arr_right)  
-=begin
-    if arr_left.sum >= arr_right.sum tnen
-     return (arr_left , arr_left.sum)
-    else return (arr_right , arr_right.sum)
-    end
-=end	
+    if arr_left.sum >= arr_right.sum 
+     return (arr_left) , arr_left.sum
+    else return (arr_right) #arr_right.sum)
+    end	
 end
 def max_arr(arr)
-	arr_help = []
+	arr_help =[]
 	l = arr.size-1
 	i = 2
-	arr_sum = arr[0]	
+	arr_sum = arr[0]
 		l.times do
 		    if arr_sum < arr.slice(0,i).sum then
 		    	arr_sum = arr.slice(0,i).sum
 		    	arr_help = arr.slice(0,i)
-		   	end	
-		 	i +=1 
+		    	i +=1
+		   	end
+		   	i +=1 
 		end
-    arr_help
+	arr_help.sum
 end
 
 
- max_Sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4 ])
+
+#print max_Sequence([-2, 1, -7, 4, -10, 2, 1, 5, 4]
+print max_arr([ -2, -1, 5, 4])
